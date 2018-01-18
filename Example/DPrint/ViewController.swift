b9db2e7f8cde259c10ee5c01dddf3cec1b24e3fc
Georgios.Erakleous <georgios.erakleous@nimbletank.com>
2018-01-18 11:14:00 +0000

    Added example

--- Example/DPrint/ViewController.swift
+++ Example/DPrint/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import DPrint
+import DPrint
 
 class ViewController: UIViewController {
 
@@ -15,14 +15,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 		
-//		DPrint.log("blah", ["item", "item 1"], ["item": 6, "item2": 8], 9)
-//
-//		DPrint.log("Testing log")
-//		DPrint.logText([2, 4], "text")
-//		DPrint.logDebug("A debug message")
-//		DPrint.logError("An error message")
-//		DPrint.logFixMe("Fix me please")
-//		DPrint.logWarning("Please be careful")
+		DPrint.log("blah", ["item", "item 1"], ["item": 6, "item2": 8], 9)
+
+		DPrint.log("Testing log")
+		DPrint.logText([2, 4], "text")
+		DPrint.logDebug("A debug message")
+		DPrint.logError("An error message")
+		DPrint.logFixMe("Fix me please")
+		DPrint.logWarning("Please be careful")
     }
 
     override func didReceiveMemoryWarning() {
